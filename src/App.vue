b492d4ed0080340f47d0cf6456c715c1de9bdcb8
sabanr <sabanr@users.noreply.github.com>
2021-07-10 11:23:00 -0300

    inject and provide!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Logon testing</h1>
+    <h1>Logon testing. {{ genius }} is a genius</h1>
     <div v-if="isLoggedOn">
       <h1>User data: {{ userData }}</h1>
       <button @click="LogOff">Log Off</button>
@@ -12,38 +12,17 @@
 </template>
 
 <script>
-import { ref } from "vue";
-import * as msal from "@azure/msal-browser";
+import { ref, inject } from "vue";
 
 export default {
-  async setup() {
+  setup() {
     const isLoggedOn = ref(false);
     const userData = ref("");
-    const t = ref("");
+    const genius = ref("N/A");
 
-    const msconfig = {
-      auth: {
-        clientId: "9fd0dafb-3646-47cc-b6a8-05ca78e5fff8",
-        authority:
-          "https://login.microsoftonline.com/06671189-dfc0-40b6-a063-435ac5cc7b24",
-      },
-      system: {
-        loggerOptions: {
-          loggerCallback(loglevel, message, containsPii) {
-            console.log(message);
-          },
-          piiLoggingEnabled: false,
-          logLevel: msal.LogLevel.Verbose,
-        },
-      },
-    };
-    const auth = new msal.PublicClientApplication(msconfig);
-    const authResponse = await auth.acquireTokenSilent({});
-
-    isLoggedOn.value = authResponse.account.isLoggedOn;
+    genius.value = inject("genius");
 
     const LogOn = async () => {
-      const authResponse = await auth.loginRedirect({});
       isLoggedOn.value = !isLoggedOn.value;
     };
 
@@ -52,6 +31,7 @@ export default {
     };
 
     return {
+      genius,
       isLoggedOn,
       userData,
       LogOn,
--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, provide } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import * as msal from "@azure/msal-browser";
+
+const msconfig = {
+  auth: {
+    clientId: "9fd0dafb-3646-47cc-b6a8-05ca78e5fff8",
+    authority:
+      "https://login.microsoftonline.com/06671189-dfc0-40b6-a063-435ac5cc7b24",
+  },
+  cache: {
+    cacheLocation: "localStorage",
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback(loglevel, message, containsPii) {
+        console.log(`${loglevel} - ${message}`);
+      },
+      piiLoggingEnabled: false,
+      logLevel: msal.LogLevel.Trace,
+    },
+  },
+};
+
+const msalClientApp = new msal.PublicClientApplication(msconfig);
+
+const app = createApp(App);
+app.provide("msalClientApp", msalClientApp);
+app.provide("genius", "Roberto");
+app.mount("#app");
